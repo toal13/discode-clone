1a99b2d0a4d1a08328634bd18cb4ea1693a7480b
toal13 <tomoyo.tomoyo.m@gmail.com>
2024-08-08 17:11:01 +0200

    update chat header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
+import Chat from "./components/chat/Chat";
 
 function App() {
   return (
     <div className="App">
       {/* side bar */}
       <Sidebar />
-      {/* home */}
+      {/* chat */}
+      <Chat />
     </div>
   );
 }
